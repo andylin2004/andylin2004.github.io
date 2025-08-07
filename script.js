bc46ca94181fc193a3cd02449d69a8ba2c68ecae
Andy Lin <andy.lin.2004@outlook.com>
2025-08-07 16:48:32 -0500

    fix typo

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let navbar_links = document.getElementById("navbar-links");
     if (window.innerWidth > 500) {
-        navbar_links.classList.add("supress-animations");
+        navbar_links.classList.add("suppress-animations");
     }
 })
 
@@ -16,11 +16,11 @@ window.addEventListener("resize", function () {
 
     this.clearTimeout(timeoutFuncID);
     if (this.window.innerWidth > 500) {
-        navbar_links.classList.add("supress-animations");
+        navbar_links.classList.add("suppress-animations");
         document.getElementById("show-menu-btn").checked = false;
     } else {
         timeoutFuncID = setTimeout(function () {
-            navbar_links.classList.remove("supress-animations");
+            navbar_links.classList.remove("suppress-animations");
         }, 500);
     }
     
